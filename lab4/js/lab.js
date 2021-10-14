4ed28cf26a5cf64e28b913544b73cd70a5fe4135
aeshghi <aeshghi@ucsc.edu>
2021-10-14 02:56:49 -0700

    lab4 index.html, CSS, created

---
+++ lab4/js/lab.js
@@ -0,0 +1,69 @@
+/**
+* This program is to design a website for buy and reselling of video games.
+*
+* This website will allow the user to choose the video game that they have in
+* mind either by choosing it straight from the main page or filtering the games
+* throughout the attributes of the game, they can bid on a game or offer a
+* reselling price for the video game.
+*
+*
+* @link: https://aeshghi.github.io/art101/lab4/
+*
+* @file: The files art101/lab4/css & art101/lab4/img art101/lab4/js
+*        art101/lab4/index.html
+*
+* @auther Ali Eshghi and Amirarsalab Valipour
+*
+* @since 10/13/2021
+*/
+
+
+//
+// Creating a web-site for buying and reselling video games:
+//  Display the name and the image of all the games available
+//    Displays portion of attributes of the game, such as price and age-range
+//      Clicking on each banner opens a new window about the game only
+//      Displays the picture of the game, some information about the game,
+//      and displays all the attributes of the game such as the price,
+//      age range, creator brand.
+//      Displays the price section with three parts of price for brand new,
+//      The lowest price ask to buy a certified used game, and the highest
+//      bid from a buyer
+//
+// Display banner on top with limited members of new available and trending products
+// Contains an Horizontal scroll
+//
+// Displays help bar at the very top containing the following options:
+//   About
+//   Help
+//   News
+//  Login/Signup
+//
+// Displays a shopping help bar containing the different type of products i.e:
+//  Video games
+//  Collectibles
+//  Accessories
+//  Add-ons
+//  Cheat codes/ Game assist
+//
+// Displays a bar with filters that displays the video games sorted in groups
+// based on the preference attribute
+//
+// Displays category option for the products based on the manufacturer and
+// the brand at the bottom
+//    Different logos of different brands, containing the games with the same
+//    brand attribute as the brand section they’re assigned to
+//
+// Display the credit for the creator of the website,
+// the social media account addresses and link, contact information and
+// frequently asked questions
+//
+//
+// Creating a Tic-Tac-Toe game:
+// 1. Print the board before each turn
+// 2. Accept user input
+// 3. Validate user move
+// 4. Evaluate possible moves
+// 5. Choose computer move
+// 6. Test of win or draw condition
+//
