c3d9bfb064e92d68d6c10dad418f5b5d7601c70a
aeshghi <aeshghi@ucsc.edu>
2021-10-25 22:37:24 -0700

    Lab 5 javascript, html, css(one job)
    the CSS needs more work

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/********************************
+* Author: Ali Eshghi (aeshghi@ucsc.edu)
+* Created: Oct 19, 2021
+* License: public domain
+**********************************/
+
+//Define variables
+make = "Jeep";
+model = "Renegade";
+color = "Black";
+year = 2018;
+
+//calculate
+age = 2021 - year;
+
+//Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
